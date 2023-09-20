c35054a31e8c5482d7de291dc1d9389069d78cfd
LeDeNew <w.rochalves@gmail.com>
2023-09-20 17:28:15 -0300

    Release

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Codigos/Trabalho Final/Jogo-queda-de-estrelas
+# Build directory: D:/Codigos/Trabalho Final/Jogo-queda-de-estrelas/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
