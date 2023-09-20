41cd406919ca0615012f8f9564a76cc5755d2da3
LeDeNew <138792703+LeDeNew@users.noreply.github.com>
2023-09-20 17:37:42 -0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Codigos/Trabalho Final/Jogo-queda-de-estrelas
-# Build directory: D:/Codigos/Trabalho Final/Jogo-queda-de-estrelas/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
